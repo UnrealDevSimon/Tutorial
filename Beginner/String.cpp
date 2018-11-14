011507c31480b546595c091fea9b70fd73d6259a
Simon Johansson <simjohansson93gmail.com>
2018-11-14 20:09:32 +0100

    Tested length

--- Beginner/String.cpp
+++ Beginner/String.cpp
@@ -3,9 +3,57 @@
 
 using namespace std;
 
+//int main()
+//{
+//	string name;
+//	cout << "Who are you?";
+//	cin >> name;
+//	string greeting = "Hello, " + name;
+//	if (name == "Simon")
+//	{
+//		greeting += ", I know you!";
+//	}
+//	cout << greeting << endl;
+//
+//	int l = greeting.length();
+//	cout << "\"" + greeting + "\" is "
+//		<< l << " characters long." << endl;
+//	auto space = greeting.find(' ');
+//	string beginning = greeting.substr(space + 1);
+//	cout << beginning << endl;
+//	if (beginning == name)
+//	{
+//		cout << "expected result." << endl;
+//	}
+//
+//	return 0;
+//}
+
 int main()
 {
-	cout << "Hello World!";
+	string word1;
+	string word2;
+
+	cout << "Plz enter a word: ";
+	cin >> word1;
+	cout << "Plz enter another word: ";
+	cin >> word2;
+
+	if (word1.length() < word2.length())
+	{
+		cout << "The word length of " + word1 + " less than "
+			+ word2 + "!";
+	}
+	if (word1.length() > word2.length())
+	{
+		cout << "The word length of " + word1 + " greater than "
+			+ word2 + "!";
+	}
+	if (word1.length() == word2.length())
+	{
+		cout << "The word length of " + word1 + " and "
+			+ word2 + " is the same!";
+	}
 
 	return 0;
 }
